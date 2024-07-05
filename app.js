8664a8debced4bff25b54468b973bd219beedae0
Cristh72 <guerrerocristhian.coder@gmail.com>
2024-07-05 21:29:22 +0200

    Empezar estilos 2da mitad pagina y nuevos metodos js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let palabraEncriptada = encriptarTexto();
 let palabraCopia = "";
 
 function encriptarTexto() {
-    let palabra = document.getElementById("input__text").value;
+    let palabra = document.getElementById("ingreso__palabra").value;
     let construccionPalabaraEncriptada = palabra
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
@@ -11,11 +11,17 @@ function encriptarTexto() {
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
     mostrarResultado(construccionPalabaraEncriptada);
+    limpiarCaja();
+    if (document.getElementById('resultado__texto').value === ""){
+        desactivarBotonDesencriptar();
+    }else {
+        activarBotones();
+    }
     return;
 }
 
 function desencriptarTexto() {
-    let palabra = document.getElementById("input__text").value;
+    let palabra = document.getElementById("ingreso__palabra").value;
     let construccionPalabaraEncriptada = palabra
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
@@ -23,6 +29,8 @@ function desencriptarTexto() {
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
     mostrarResultado(construccionPalabaraEncriptada);
+    desactivarBotonDesencriptar();
+    limpiarCaja();
     return;
 }
 
@@ -37,5 +45,21 @@ function copiarTexto() {
     navigator.clipboard.writeText(elementoHTML);
 }
 
+function activarBotones() {
+   //document.getElementById('boton__copiar').removeAttribute('hidden');
+    let botonDesencriptar = document.getElementById('desencriptar__texto');
+    botonDesencriptar.disabled = false;
+    botonDesencriptar.style.background = 'inherit';
+}
+
+function desactivarBotonDesencriptar() {
+    let botonDesencriptar = document.getElementById('desencriptar__texto');
+    botonDesencriptar.disabled = true;
+    botonDesencriptar.style.background = 'var(--color--boton--desencriptar)';
+}
+
+function limpiarCaja() {
+    document.querySelector('#ingreso__palabra').value = '';
+}
 
 
